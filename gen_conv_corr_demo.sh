5c457d591684434497ef27b87d79361f5f2ff4bc
Piyush Singh <mrsinghpiyush@gmail.com>
2017-05-14 12:37:22 +0100

    Reduce delay

--- gen_conv_corr_demo.sh
+++ gen_conv_corr_demo.sh
@@ -17,7 +17,7 @@ convert -density 600 pdf/corr_numerical_no_padding_no_strides_07.pdf -flatten -r
 ./bin/produce_figure numerical corr_numerical_no_padding_no_strides 8 --input-size=5 --output-size=3 --padding=0 --kernel-size=3 --stride=1 --mode=convolution 
 convert -density 600 pdf/corr_numerical_no_padding_no_strides_08.pdf -flatten -resize 25% png/corr_numerical_no_padding_no_strides_08.png
 convert -delay 200 -loop 0 -layers Optimize +map -dispose previous png/corr_numerical_no_padding_no_strides_00.png png/corr_numerical_no_padding_no_strides_01.png png/corr_numerical_no_padding_no_strides_02.png png/corr_numerical_no_padding_no_strides_03.png png/corr_numerical_no_padding_no_strides_04.png png/corr_numerical_no_padding_no_strides_05.png png/corr_numerical_no_padding_no_strides_06.png png/corr_numerical_no_padding_no_strides_07.png png/corr_numerical_no_padding_no_strides_08.png gif/corr_numerical_no_padding_no_strides.gif
-gifsicle --batch -d 250 -O3 gif/corr_numerical_no_padding_no_strides.gif
+gifsicle --batch -d 200 -O3 gif/corr_numerical_no_padding_no_strides.gif
 
 
 
@@ -40,7 +40,7 @@ convert -density 600 pdf/conv_numerical_no_padding_no_strides_07.pdf -flatten -r
 ./bin/produce_figure convolution conv_numerical_no_padding_no_strides 8 --input-size=5 --output-size=3 --padding=0 --kernel-size=3 --stride=1 --mode=convolution 
 convert -density 600 pdf/conv_numerical_no_padding_no_strides_08.pdf -flatten -resize 25% png/conv_numerical_no_padding_no_strides_08.png
 convert -delay 200 -loop 0 -layers Optimize +map -dispose previous png/conv_numerical_no_padding_no_strides_00.png png/conv_numerical_no_padding_no_strides_01.png png/conv_numerical_no_padding_no_strides_02.png png/conv_numerical_no_padding_no_strides_03.png png/conv_numerical_no_padding_no_strides_04.png png/conv_numerical_no_padding_no_strides_05.png png/conv_numerical_no_padding_no_strides_06.png png/conv_numerical_no_padding_no_strides_07.png png/conv_numerical_no_padding_no_strides_08.png gif/conv_numerical_no_padding_no_strides.gif
-gifsicle --batch -d 250 -O3 gif/conv_numerical_no_padding_no_strides.gif
+gifsicle --batch -d 200 -O3 gif/conv_numerical_no_padding_no_strides.gif
 
 
 
